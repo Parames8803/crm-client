7a8e59013722be9754198e698a670a9f4916270d
Parames8803 <paramesh.e.cse.2021@snsce.ac.in>
2024-11-08 14:22:47 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this to suppress warnings
+  },
   server: {
-    port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://crm-server-0wml.onrender.com",
         changeOrigin: true,
       },
     },
   },
 });
+
